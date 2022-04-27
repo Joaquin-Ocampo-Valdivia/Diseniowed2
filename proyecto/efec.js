b76179589838bd4a9a2809cbddef95507dca527d
Joaquin-Ocampo-Valdivia <cb.joaquin.ocampo.v@upds.net.bo>
2022-04-26 20:56:25 -0400

    update

---
+++ proyecto/efec.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+	// Efecto Menu
+	$('.men a').each(function(PROYECTO, elemento){
+		$(this).css({
+			'top': '-100px'
+		});
+		$(this).animate({
+			'top': '0px'
+		},1000 + (PROYECTO * 1000));
+	});
+    // Efecto Header
+	if ($(window).width() > 800){
+		$('.men a').css({
+			opacity: 0,
+			marginTop: 0
+		});
+
+		$('.men a').animate({
+			opacity: 1,
+			marginTop: '5px'
+		},1000);
+	}
+	//var top = $('#top').offset().top;
+	//var	contacto = $('#contacto').offset().top;
+	var	opi = $('#opi').offset().top;
+	//var	snn = $('#sn').offset().top;
+        
+        
+// Boton Acerca de
+	$('#opi').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: 380
+		}, 500);
+	});
+});
